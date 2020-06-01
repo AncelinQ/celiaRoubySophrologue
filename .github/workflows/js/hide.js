bdfdac4bd89388bc6c4dae5a32e04290957e2ad6
Ancelin <57001956+AncelinQ@users.noreply.github.com>
2020-06-01 09:32:44 +0200

    Delete hide.js

--- .github/workflows/js/hide.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-//ON CACHE LE FORMULAIRE DE PRISE DE RDV ET D'AJOUT DE MESSAGE LORS DE LA PRISE DE RENDEZ-VOUS AVANT//
-$(function() {
-    $('#userInfoForm').hide();
-    $(".rdvMessage").hide();
-});
