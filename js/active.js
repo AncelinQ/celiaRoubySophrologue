41ca7e22c91591257911948e3737b6c586aa113c
Ancelin Quinton <ancelin.quinton@gmail.com>
2020-06-21 11:49:03 +0200

    point virgule rajouté

--- js/active.js
+++ js/active.js
@@ -1,8 +1,10 @@
-'use strict';
+"use strict";
 //FONCTION D'AJOUT DE CLASSE POUR METTRE EN VALEUR L'ONGLET DANS LA NAV CORRESPONDANT À LA PAGE OUVERTE//
 $(function () {
     //ON RÉCUPÈRE D'ABORD LE NOM DE L'ONGLET DANS L'URL, SITUÉ APRÈS LE DERNIER "/"//
-    var adresse = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
+    var adresse = window.location.href.substr(
+        window.location.href.lastIndexOf("/") + 1
+    );
     //ENSUITE ON REGARDE CHAQUE ELEMENT "<a>" DANS LA NAV//
     $("#headerNav ul li a").each(function () {
         //SI UN DES ELEMENTS A L'ATTRIBUT "href" (DONC LE LIEN VERS LEQUEL IL RENVOIE) QUI CORRESPOND À LA VARIABLE "url", ON LUI AJOUTE LA CLASSE ACTIVE//
@@ -10,18 +12,25 @@ $(function () {
             $(this).addClass("active");
         }
         //ON AJOUTE LA CLASSE ACTIVE À L'ONGLET PRINCIPAL "Mes Spécialisation" EN PLUS DE L'AJOUT SUR L'ONGLET SECONDAIRE//
-        if (adresse === 'violences.php' || adresse === 'rapport-au-corps.php' || adresse === 'gestion-de-la-douleur.php') {
-            $('#specialisationsNav > a').addClass("active");
+        if (
+            adresse === "violences.php" ||
+            adresse === "rapport-au-corps.php" ||
+            adresse === "gestion-de-la-douleur.php"
+        ) {
+            $("#specialisationsNav > a").addClass("active");
         }
         //MEME CHOSE ICI SAUF QU'IL S'AGIT DE DIFFÉRENTES SECTIONS D'UNE MEME PAGE//
-        if (adresse === 'infos-pratiques.php#tarifs' || adresse === 'infos-pratiques.php#seances' || adresse === 'infos-pratiques.php#cabinet') {
-            $('#infosPratiques > a').addClass("active");
+        if (
+            adresse === "infos-pratiques.php#tarifs" ||
+            adresse === "infos-pratiques.php#seances" ||
+            adresse === "infos-pratiques.php#cabinet"
+        ) {
+            $("#infosPratiques > a").addClass("active");
         }
-
     });
     //ON ENLÈVE LA CLASSE ACTIVE AUX ONGLETS SECONDAIRES AVANT DE L'AJOUTER À CELUI SUR LEQUEL ON A CLIQUÉ //
-    $('.subMenu li a').click(function () {
-        $('.subMenu li a').removeClass('active');
-        $(this).addClass('active');
-    })
+    $(".subMenu li a").click(function () {
+        $(".subMenu li a").removeClass("active");
+        $(this).addClass("active");
+    });
 });
